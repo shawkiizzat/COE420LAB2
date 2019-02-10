0402cf4f2401ea5bb06883caf23fc66e9ba845f0
shawkiizzat <b00059726@aus.edu>
2019-02-10 16:32:35 +0400

    Initial commit

---
+++ src/coe420lab2/COE420LAB2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab2;
+
+/**
+ *
+ * @author b00059726
+ */
+public class COE420LAB2 {
+
+    public static void addFeatureA()
+    {
+        System.out.println("Feature A added ");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        addFeatureA();
+    }
+    
+}
